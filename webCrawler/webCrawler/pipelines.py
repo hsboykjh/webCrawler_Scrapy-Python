0073119d25287683107ea0f695630f2a15bd86f7
hsboykjh <hsboykjh@gmail.com>
2016-02-27 01:21:04 +1100

    MongoDB pipelines modified
    comment added
    MongoDB env modified

--- webCrawler/webCrawler/pipelines.py
+++ webCrawler/webCrawler/pipelines.py
@@ -17,31 +17,35 @@ from scrapy.conf import settings
 from scrapy.exceptions import DropItem
 from scrapy import log
 
-# See: MongoDB reference http://doc.scrapy.org/en/latest/topics/item-pipeline.html?highlight=mongodb
+# See: Scrapy MongoDB reference http://doc.scrapy.org/en/latest/topics/item-pipeline.html?highlight=mongodb
 
 class MongoDBPipeline(object):
     
-    collection_name = 'scrapy_items'
-    
     def __init__(self):
+        # connect MongoDB (local or server)
+        # id/passwd for (Compose)access was defined at setting.py
+        # MONGODB_SERVER is composed of userID/password and serviceURL at setting.py
+        logging.info("MONGODB_SERVER info : %s ",settings['MONGODB_SERVER'])
         connection = pymongo.MongoClient(
                                          #define MONGODB ENV (MONGODB_SERVER,MONGODB_PORT,MONGODB_DB,MONGODB_COLLECTION)  at setting.py
                                          #Currntly Local MongoDB set for basic test
-                                         #settings['MONGODB_SERVER'],
-                                         #settings['MONGODB_PORT']
-                                         "mongodb://hsboykjh:kjh3131@aws-us-east-1-portal.14.dblayer.com", 10095
-                                         #/admin -u hsboykjh -p kjh3131"
-        )
+                                         settings['MONGODB_SERVER'],
+                                         settings['MONGODB_PORT']
+                                         )
             
         db = connection[settings['MONGODB_DB']]
         self.collection = db[settings['MONGODB_COLLECTION']]
 
     def process_item(self, item, spider):
         
+        ###### To-do Duplicate item check ######
+        
+        ########################################
+        
+        
         # insert item into MongoDB
         self.collection.insert({'keyword' : item['keyword'] , 'headline' : item['headline'] , 'article_url' : item['article_url'] , 'article_text' : item['article_text']})
 
         #logging.info("PIPELINES ITEM info : %s || %s || %s || %s" , item['article_url'],item['article_text'],item['keyword'],item['headline'])
         
-        #self.collection.insert({'keyword' : "field2" , 'headline' : "field12" , 'article_url' : "field3" , 'article_text' : "field4"})
         return item
